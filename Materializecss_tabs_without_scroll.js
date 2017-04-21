b63b7132ea35991bb0c8402fd15ad35d001e2486
Shubhanil Bag <shubhanil.bag@gmail.com>
2017-04-21 18:50:11 +0530

    Update Materializecss_tabs_without_scroll.js

--- Materializecss_tabs_without_scroll.js
+++ Materializecss_tabs_without_scroll.js
@@ -7,5 +7,5 @@ $('#mytabs').tabs({onShow:function(tab){
     var pos = $('#mytabs a.active')[0].parentElement.getBoundingClientRect().left;
     var sw = $('#mytabs a.active').width()/2;
     pos = pos+sw;
-    $('#mytabs').scrollLeft($('#tabs-swipe-demo').scrollLeft()-(cW-pos));
+    $('#mytabs').scrollLeft($('#mytabs').scrollLeft()-(cW-pos));
   }});
